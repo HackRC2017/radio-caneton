d7e947d56d6e6d59fc86dbe637ca790e95081933
Francis Bergin <bergin.francis@gmail.com>
2017-03-25 20:00:02 -0400

    Get articles from main module

--- radio_caneton/__main__.py
+++ radio_caneton/__main__.py
@@ -1,11 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import pymongo
 
-from . import radio_caneton
+from . import radio_caneton as rc
+
+
+client = pymongo.MongoClient()
+db = client.articles
 
 
 def update_db():
-    pass
+    articles = rc.get_articles()
+
 
 scheduler = BlockingScheduler()
-scheduler.add_job(update_db, 'interval', seconds=5)
+scheduler.add_job(update_db, 'interval', minutes=5)
 scheduler.start()
